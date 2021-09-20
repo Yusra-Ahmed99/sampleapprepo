a6cd57eacf588ab53a138167efb70aba691c0c83
Yusra-Ahmed99 <yusra ahmed>
2021-09-20 11:09:47 +0530

    updated bootclass method from workspace2

--- newyus-repo/src/main/java/org/cap/demo/bootclass.java
+++ newyus-repo/src/main/java/org/cap/demo/bootclass.java
@@ -5,6 +5,14 @@ public class bootclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("HELLOOO");
+show();
 	}
-
+public static void show()
+{   int[] arr= {1,2,3,4};
+    for(int i=0;i<=10;i++)
+    {
+    	System.out.println(i);
+    }
+	System.out.println("showing show method");
+}
 }
