bd227db105dc24e4e1dc31de64467b0d0fde3f3b
Yusra-Ahmed99 <yusra ahmed>
2021-09-20 11:52:26 +0530

    workspace4 bootclass updated

--- newyus-repo/src/main/java/org/cap/demo/bootclass.java
+++ newyus-repo/src/main/java/org/cap/demo/bootclass.java
@@ -9,11 +9,10 @@ show();
 	}
 public static void show()
 {   int[] arr= {1,2,3,4};
-    int sum=0;
     for(int i=0;i<=10;i++)
     {
     	System.out.println(arr[i]);
-    	sum+=i;
+    	System.out.println(i);
     }
 	System.out.println(sum);
 }
